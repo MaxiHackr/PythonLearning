e0bed002851d7e0fe3d7ad1797422a3e623f47c5
Matthew Mckeen <104150013+MaxiHackr@users.noreply.github.com>
2022-04-25 15:51:42 +0000

    BaristaBot
    added Security Feature (If your name is Ben you will get G checked)
    Updated Pricing
    Added "Would you like anything else" input
    Added new menu item
    Added Total
    Any feature recommendations would be greatly appreciated. Also any criticism and feedback welcome!

--- main.py
+++ main.py
@@ -5,25 +5,94 @@ print("Welcome to Maxi™ Coffee Cabin!\n" + "I am Barista_01\n")
 
 name = input("What is your name?\n")
 
-print("\nHello " + name + ",\n\n" + "Our Menu today is:\n")
+if name == "Ben" or name == "Patricia" or name == "Loki" or name == "Jonny":
+    evil_status = input("Are you evil?\n\n")
+    good_deeds = int(input("How many good deeds have you done today?\n\n"))
+    if evil_status == "Yes" and good_deeds < 4:
+        print("You're not welcome here " + name + ". Get out!!")
+        exit()
+    else:
+        print("Oh you're one of the good " + name +
+              "'s! You are welcome to stay")
+else:
+    print("\nHello " + name + ",\n\n" + "Our Menu today is:\n")
 
-menu = """Americano
-Latte
-Cappuccino
-Espresso
-Hot Chocolate"""
+price = 0
+menu = """
+£2.00 Americano
+£2.20 Latte
+£2.20 Cappuccino
+£1.80 Espresso
+£2.30 Hot Chocolate
+£2.50 Iced Latte
+
+Syrups: 50p /(per pump)"""
 
 print(menu)
 
-drink = input("\nWhat can I get for you?\n\n")
+order = input("\nWhat can I get for you?\n\n")
 
+yes = ("Yes" or "yes")
+quantity2 = 0
 
-print("\nOkay " + name + ", we will have that " + drink + " ready in just a moment!")
+if order == "Americano":
+    price = 2.00
+elif order == "Latte":
+    price = 2.20
+elif order == "Cappuccino":
+    price = 2.20
+elif order == "Espresso":
+    price = 1.80
+elif order == "Hot Chocolate":
+    price = 2.30
+elif order == "Iced Latte":
+    price = 2.50
+else:
+    print("We dont have that on our menu.")
+    price = 0
+    exit()
 
-price_latte = "£1.05"
-price_americano = "£1.00"
-price_cappuccino = "£1.10"
+quantity = input("\nHow many would you like?\n")
+##Syrups##
+syrup = input("\nWould you like any syrup added in? Answer (Yes/No)\n\n")
+syrup_price = 0.50
+syrup_menu = """\n-=Syrup Menu=-
+Hazelnut
+Chocolate
+Vanilla
+Caramel
+Salted Caramel
+White Chocolate
+"""
 
-print("For your " + drink + " that will be " + price_latte)
+if syrup == "Yes" or syrup == "yes" or syrup == "Y" or syrup == "y":
+    print(syrup_menu)
+    syrup = input("Which one would you like\n\n")
+elif syrup == "No" or syrup == "no" or syrup == "N" or syrup == "n":
+    syrup_price = 0
 
+##2nd Order/Do you want anything else?
+order2 = input("\nWould you like anything else? Answer with (Yes or No)\n\n")
 
+if order2 == yes:
+    print(menu)
+    order2 = input("\nWhat can I get for you?\n")
+    quantity2 = input("\nHow many would you like\n")
+    print("\nWe will now proceed to payment.\n")
+else:
+    print("\nI will now direct you to payment\n")
+    order2 = ""
+
+quantity = int(quantity) + int(quantity2)
+total = price * int(quantity) + int(syrup_price)
+
+print("Your total is: £" + str(total))
+
+print("\nThank you " + name + ", we will have your " + order + "(s) and " +
+      order2 + "(s) ready in just a moment!\n\n")
+
+print("Please collect your drink(s) at the counter")
+
+print(str(quantity) + " " + order + "(s)")
+print(str(quantity2) + " " + order2 + "(s)")
+print("£" + str(total))
